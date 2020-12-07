13068fdf23b285a3672d1ad2b594be2257df1f80
Hozefa <hozefam@gmail.com>
2020-12-07 14:14:51 +0300

    Final changes

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        headings: ['Playfair Display'],
+        description: ['Montserrat'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
